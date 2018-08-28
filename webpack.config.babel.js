b93610d3445adf14c5bf63c8ef3140cb01ad9791
Emil Martinov <martinov.emil@gmail.com>
2018-08-28 16:59:50 +0200

    Production build with vue.min.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,62 +1,63 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
 
 module.exports = {
-  entry: './src/app',
+  entry: "./src/app",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [  'style-loader',
-                { loader: 'css-loader', options: { importLoaders: 1 } },
-                'postcss-loader' 
-            ]
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader"
+        ]
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"] 
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
         test: /\.(html)$/,
         use: {
-          loader: 'html-loader',
+          loader: "html-loader",
           options: {
-            attrs: [':data-src']
+            attrs: [":data-src"]
           }
         }
       }
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, './dist'),
-    compress : true,
+    contentBase: path.join(__dirname, "./dist"),
+    compress: true
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-        title: 'Development',
-        template: './src/index.html' // Normally creates a generic HTML for js file. But with template we can feed in our own HTML.
-      }),
-      new HtmlWebpackExternalsPlugin({
-        externals: [
-          {
-            module:'vue', //Example for CDN
-            entry: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
-          }
-        ]
-      })
-  ] 
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./src/index.html" // Normally creates a generic HTML for js file. But with template we can feed in our own HTML.
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: "vue", //Example for CDN
+          entry: "https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"
+        }
+      ]
+    })
+  ]
 };
